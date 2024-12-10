20cc94c0379295af062b56483f56e2059623ae6d
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-12-10 18:06:21 +0500

    learning

---
+++ lec-1/C++ DSA foundation/operators.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 8;
+    cout << sizeof(a) << endl;
+
+    char name = 'a';
+    cout << sizeof(name) << endl;
+
+    bool flag;
+    a == name ? flag = true : flag = false;
+    cout << flag << endl;
+
+    cout << (&a) << endl;
+
+    return 0;
+}
