d737f262754f9ddc7d432d65cbfec19516a34280
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-12-12 04:37:25 +0500

    learning

---
+++ lec-1/C++ DSA foundation/data-type-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // cout << "Hello world!";
+    cout << "Enter a number:";
+
+    int num;
+    cin >> num;
+    cout << "The entered number is: " << num << endl;
+
+    return 0;
+}
