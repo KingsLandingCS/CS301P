3fff517329c8837a63c75345b6d17161868524e9
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2025-01-03 23:02:18 +0500

    learning

---
+++ C++ DSA foundation/functions/squareRoo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int square(int x)
+{
+    return x * x;
+}
+
+int main()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        cout << square(i) << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
