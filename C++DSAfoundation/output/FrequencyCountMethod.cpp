f16cb03af46c4ee923cf702841a08dbd054c1636
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2025-01-14 08:51:03 +0500

    learning

---
+++ C++DSAfoundation/output/FrequencyCountMethod.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+// int main(A, B, n)
+// {
+//     for (i = 0; i < n; i++)
+//     {
+//         for (j = 0; j < n; j++)
+//         {
+//             C[i, j] = 0;
+//             for (k = 0; k < n; k++)
+//             {
+//                 C[i, j] = C[i, j] + A[i, k] + B[k, j];
+//             }
+//         }
+
+//         return 0;
+//     }
+// }
