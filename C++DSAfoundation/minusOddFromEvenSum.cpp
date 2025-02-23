1e81ed343a4c6815fb88abb1914732af35239959
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2025-02-23 21:01:20 +0500

    learning

---
+++ C++DSAfoundation/minusOddFromEvenSum.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> arr;
+    cout << "How many elements in your array? ";
+    int n;
+    cin >> n;
+
+    cout << "Enter your " << n << "elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        int num;
+        cin >> num;
+
+        // Handles invalid input
+        if (cin.fail())
+        {
+            cout << "Invalid input!";
+            return 0; // stops execution immediately (anything after it is skipped).
+        }
+
+        arr.push_back(num);
+    }
+
+    // Edge case if no numbers found
+    if (arr.empty())
+    {
+        cout << "No values found!" << endl;
+        return 0;
+    }
+
+    int evenIndexSum = 0;
+    int oddIndexSum = 0;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (i % 2 == 0)
+        {
+            evenIndexSum += arr[i];
+        }
+        else if (i % 2 != 0)
+        {
+            oddIndexSum += arr[i];
+        }
+    }
+
+    cout << "Even Sum: " << evenIndexSum << endl;
+    cout << "Odd Sum:" << oddIndexSum << endl;
+
+    cout << "Differnce (evenSum - oddSum): " << evenIndexSum - oddIndexSum << endl;
+}
