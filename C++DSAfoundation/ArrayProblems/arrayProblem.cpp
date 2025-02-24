81d21b9833cb000a602d841cbad9ec36a55dc097
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2025-02-24 23:02:50 +0500

    learning

--- C++DSAfoundation/ArrayProblems/arrayProblem.cpp
+++ C++DSAfoundation/ArrayProblems/arrayProblem.cpp
@@ -8,17 +8,21 @@ int main()
 
     int size = 5;
     int pairs = 0;
+
     for (int i = 0; i < size; i++)
     {
-        for (int j = i + 1; j < size; i++)
+        for (int j = i + 1; j < size; j++)
         {
             if (arr[i] + arr[j] == targetSum)
             {
+                cout << "Pair found: " << arr[i] << " (index " << i << ") + "
+                     << arr[j] << " (index " << j << ") = " << targetSum << endl;
                 pairs++;
             }
         }
     }
-    cout << pairs << endl;
+
+    cout << "Total pairs found: " << pairs << endl;
 
     return 0;
 }
