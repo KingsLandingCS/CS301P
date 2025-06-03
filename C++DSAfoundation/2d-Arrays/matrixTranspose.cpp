726ca0d27d3d110f8edafa691082d1f46cc2ebb8
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2025-06-03 17:38:13 +0500

    learning

--- C++DSAfoundation/2d-Arrays/matrixTranspose.cpp
+++ C++DSAfoundation/2d-Arrays/matrixTranspose.cpp
@@ -15,6 +15,15 @@ int main()
         }
     }
 
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
     int transpose[m][n];
     for (int i = 0; i < m; i++)
     {
