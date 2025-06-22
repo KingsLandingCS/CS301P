ff9cf682de2b56ae0f3db71e8fd9e1dea03ef660
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2025-06-22 23:41:52 +0500

    learning

--- C++DSAfoundation/MultiDimensionalArray/leftMostOneRow.cpp
+++ C++DSAfoundation/MultiDimensionalArray/leftMostOneRow.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <vector>
+#include <climits>
 
 using namespace std;
 
-int leftMostOneRow(vector < vector<int> & V)
+int leftMostOneRow(vector<vector<int>>& V)
 {
     int leftMostOne = -1;
     int maxOnesRow = -1;
     int j = V[0].size() - 1;
 
-    while (j >= 0; &&V[0][j] == 1)
+    while (j >= 0 && V[0][j] == 1)
     {
         leftMostOne = j;
         maxOnesRow = 0;
@@ -29,8 +30,46 @@ int leftMostOneRow(vector < vector<int> & V)
     return maxOnesRow;
 }
 
+int maximumOnesRow(vector<vector<int>> &V)
+{
+    int maxOnes = INT_MIN;
+    int maxOnesRow = -1;
+    int columns = V[0].size();
+
+    for (int i = 0; i < V.size(); i++)
+    {
+        for (int j = 0; j < V[i].size(); j++)
+        {
+            if (V[i][j] == 1)
+            {
+                int numberOfOnes = columns - j;
+                if (numberOfOnes > maxOnes)
+                {
+                    maxOnes = numberOfOnes;
+                    maxOnesRow = i;
+                }
+            }
+        }
+    }
+    return maxOnesRow;
+}
+
 int main()
 {
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> vec(n, vector<int>(m));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> vec[i][j];
+        }
+    }
+
+    int res = maximumOnesRow(vec);
+    cout << res << endl;
 
     return 0;
 }
