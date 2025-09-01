fbbe36e63495d32ff5cfcbb56b80434d76b2fb9c
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2025-09-01 18:37:04 +0500

    learning

---
+++ Space-Time-Complexity/demo.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int sumInRange(int x, int y)
+{
+    int result = 0;
+    for (int i = x; i <= y; i++)
+    {
+        result += i;
+    }
+    return result;
+}
+
+int sumInRangeOptimized(int x, int y)
+{
+    int n = (y - x + 1);
+    int a = x;
+    int result = (n * (2 * a + (n - 1) * 1)) / 2;
+    return result;
+}
+
+int main()
+{
+    cout << sumInRangeOptimized(1, 100000) << endl;
+    return 0;
+}
