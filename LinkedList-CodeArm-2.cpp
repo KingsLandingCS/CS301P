b7185277a9c2d5d3dee5834b0a7aab95a6005a6f
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2025-10-14 11:40:09 +0500

    learning

---
+++ LinkedList-CodeArm-2.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int value)
+    {
+        data = value;
+        next = NULL;
+    }
+};
+
+int main()
+{
+    Node *head, *tail;
+    tail = head = NULL;
+
+    int arr[] = {2, 4, 6, 8, 10};
+    // Insert the value at end
+
+    for (int i = 0; i < 5; i++)
+    {
+
+        // Linked list is empty
+        if (head == NULL)
+        {
+            head = new Node(arr[i]);
+            tail = head;
+        }
+
+        // Linked list exits
+        else
+        {
+            tail->next = new Node(arr[i]);
+            tail = tail->next;
+        }
+    }
+
+    Node *temp;
+    temp = head;
+
+    while (temp)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+
+    cout << endl;
+}
