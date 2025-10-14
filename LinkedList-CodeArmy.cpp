658f871be7e58b2cca67aaf2fc6b021a8ef5b755
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2025-10-14 11:12:58 +0500

    learning

---
+++ LinkedList-CodeArmy.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+int main()
+{
+    Node *head;
+    head = NULL;
+
+    int arr[] = {2, 4, 6, 8, 10};
+
+    // Insert the Node at begninning
+
+    // Linked list doesn't exist
+
+    for (int i = 0; i < 5; i++)
+    {
+
+        if (head == NULL)
+        {
+            head = new Node(arr[i]);
+        }
+
+        // Linked list exits
+        else
+        {
+            Node *temp;
+            temp = new Node(arr[i]);
+            temp->next = head;
+            head = temp;
+        }
+    }
+
+    // Print the value;
+    Node *temp = head;
+
+    while (temp)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+
+    return 0;
+}
